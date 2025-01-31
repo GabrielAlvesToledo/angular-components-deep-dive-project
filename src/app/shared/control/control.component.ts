bc15d8f1213161763829fc99ab202888315d7f17
Majin <gabrielalvestoledodev@gmail.com>
2025-01-31 07:40:17 -0300

    add new shared component to form control

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from "@angular/core";
+
+@Component({
+    selector: 'app-control',
+    standalone: true,
+    templateUrl: './control.component.html',
+    styleUrl: './control.component.css'
+}) 
+
+export class ControlComponent{
+    label = input.required<string>(); 
+    
+
+}
