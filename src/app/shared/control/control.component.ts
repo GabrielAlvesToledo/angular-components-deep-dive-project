3b9ed3cee2bd63248d796927fe2cefae96a7edb5
Majin <gabrielalvestoledodev@gmail.com>
2025-02-03 06:46:15 -0300

    add the host parameter to the @component decorator, this allow me to set any attribute for my component element, like a class attribute

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -5,7 +5,10 @@ import { Component, input, ViewEncapsulation } from "@angular/core";
     standalone: true,
     templateUrl: './control.component.html',
     styleUrl: './control.component.css',
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        class: 'control'
+    }
 }) 
 
 export class ControlComponent{
