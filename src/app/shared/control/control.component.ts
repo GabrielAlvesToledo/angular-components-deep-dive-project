592ac4b4677d7a096f6f85bc5952384040bf4319
Majin <gabrielalvestoledodev@gmail.com>
2025-02-10 07:47:33 -0300

    using contentChild to hold projected content

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from "@angular/core";
+import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     selector: 'app-control',
@@ -7,12 +7,21 @@ import { Component, input, ViewEncapsulation } from "@angular/core";
     styleUrl: './control.component.css',
     encapsulation: ViewEncapsulation.None,
     host: {
-        class: 'control'
+        class: 'control',
+        '(click)': 'onClick()'
     }
 }) 
 
 export class ControlComponent{
     label = input.required<string>(); 
-    
+
+    private el = inject(ElementRef);
+
+    @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+
+    onClick(){
+        console.log("Clicked");
+        console.log(this.el);
+    }
 
 }
