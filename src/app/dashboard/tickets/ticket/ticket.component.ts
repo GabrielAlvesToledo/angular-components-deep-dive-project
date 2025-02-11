6c7b4711e6669eb3e475a33fea851efb7d8c6da0
Majin <gabrielalvestoledodev@gmail.com>
2025-02-11 07:41:50 -0300

    create the ticket component and his template and style, also output the content with @for in the tickets component

---
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-ticket',
+    standalone: true,
+    templateUrl: './ticket.component.html',
+    styleUrl: './ticket.component.css'
+})
+
+export class TicketComponent{
+
+
+}
